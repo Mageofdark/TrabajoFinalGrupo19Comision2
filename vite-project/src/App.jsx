961332252b049b3ea2b50d279866a84697fb6772
matias12131 <velazquezmatiasclass@gmail.com>
2025-06-09 19:45:40 -0300

    Instalacion de react-router y react-bootstrap + incorporacion de rutas + Agregado Layout y Home por Matias Velazquez

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Layout from './Layout'
+import Home from './Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='conteiner'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='Favoritos' element={<h1>Favoritos </h1>} />
+          <Route path='Lista-Alumnos/:id' element={<h1>Detalles </h1>} />
+          <Route path='Lista-Alumnos/:id/editar' element={<h1>Editar </h1>} />
+          <Route path='Nuevo-Producto' element={<h1> AgregarProducto </h1>} />
+          <Route path='Nosotros' element={<h1>Nosotros</h1>} />
+          <Route path='*' element={<h1>PaginaError</h1>} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
---
+++ vite-project/src/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <>
+        <h1>Bienvenido a nuestra pagina (mostrar lista)</h1>
+        </>
+    )
+}
+
+export default Home
